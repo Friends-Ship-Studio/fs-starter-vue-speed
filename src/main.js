1ab5313e0e5c7de1d2195319bca2a809a49ce7b8
Kyle Kilat <kkilat@users.noreply.github.com>
2022-09-09 22:21:07 -0700

    Set up Font Awesome (Free)
    What: Installs and sets up the free Font Awesome icons, and FontAwesomeIcon component.
    
    Why: Enables Font Awesome free icons for use with this project. Only includes free package, because this is a public repo.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import router from "./router"
 import "./assets/main.css"
 import "./assets/style.css"
 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faBolt } from "@fortawesome/free-solid-svg-icons"
+library.add(faBolt)
+
 const app = createApp(App)
 
 app.use(createPinia())
